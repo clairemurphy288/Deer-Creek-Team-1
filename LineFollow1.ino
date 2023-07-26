da3c145778190327b1510069231a04c0541b5795
Sammy White <sammytwhite9@gmail.com>
2023-07-26 16:46:58 -0600

    Something

--- LineFollow1.ino
+++ LineFollow1.ino
@@ -27,6 +27,25 @@ double angle_deg = 180.0/PI;
 float T = 0;
 double power = 0.5;
 
+// This is the "main" for arduino
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600); 
+  rgbled_67.fillPixelsBak(0, 2, 1);
+  rgbled_68.fillPixelsBak(0, 2, 1);
+  TCCR1A = _BV(WGM10);
+  TCCR1B = _BV(CS11) | _BV(WGM12);
+  TCCR2A = _BV(WGM21) | _BV(WGM20);
+  TCCR2B = _BV(CS21);
+  T = 0.7;
+  rgbled_67.setColor(0, 0, 0, 0);
+  rgbled_67.show();
+
+  rgbled_68.setColor(0, 0, 0, 0);
+  rgbled_68.show();
+}
+
+
 void motor_foward_left_run(int16_t speed)
 {
    motor_10.run(-speed);
@@ -46,98 +65,6 @@ void motor_back_right_run(int16_t speed)
 {
   motor_9.run(speed);
 }
-
-void move_control(int16_t vx, int16_t vy, int16_t vw)
-{
-  int16_t foward_left_speed;
-  int16_t foward_right_speed;
-  int16_t back_left_speed;
-  int16_t back_right_speed;
-
-  foward_left_speed = vy + vx + vw;
-  foward_right_speed = vy - vx - vw;
-  back_left_speed = vy - vx + vw;
-  back_right_speed = vy + vx - vw;
-
-  motor_foward_left_run(foward_left_speed);
-  motor_foward_right_run(foward_right_speed);
-  motor_back_left_run(back_left_speed);
-  motor_back_right_run(back_right_speed);
-}
-
-void _delay(float seconds) {
-  if(seconds < 0.0){
-    seconds = 0.0;
-  }
-  long endTime = millis() + seconds * 1000;
-  while(millis() < endTime) _loop();
-}
-
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600); 
-  rgbled_67.fillPixelsBak(0, 2, 1);
-  rgbled_68.fillPixelsBak(0, 2, 1);
-  TCCR1A = _BV(WGM10);
-  TCCR1B = _BV(CS11) | _BV(WGM12);
-  TCCR2A = _BV(WGM21) | _BV(WGM20);
-  TCCR2B = _BV(CS21);
-  T = 0.7;
-  rgbled_67.setColor(0, 0, 0, 0);
-  rgbled_67.show();
-
-  rgbled_68.setColor(0, 0, 0, 0);
-  rgbled_68.show();
-}
-void _loop() {
-}
-void lineFollow(){
-  //for (int i =0; i<4; i++){
-  //rgbled_67.setColor(i, 100, 100, 100);
-  //rgbled_67.show();
-
-  //rgbled_68.setColor(i, 100, 100, 100);
-  //rgbled_68.show();
-  //}
-  double startTime = millis();  // 5 
-  int n = -1; 
-
-  while (linefollower_63.readSensor() !=0 && linefollower_64.readSensor !=0) {
-
-    if (linefollower_63.readSensor()==1 && linefollower_64.readSensor()==1) {
-      Forward();
-    Serial.println("both"); 
-    }
-
-  else if (linefollower_63.readSensor()==1 && linefollower_64.readSensor()==0){
-    TurnRight();
-    Serial.println("right"); 
-  }
-
-  else if (linefollower_63.readSensor()==0 && linefollower_64.readSensor()==1){
-    TurnLeft();
-    Serial.println("left") ;
-  }
-
-  else (linefollower_63.readSensor()==0 && linefollower_64.readSensor()==0){
-    Stop();
-    Serial.println("neither");
-  }
-  
-}
-
-void ObstacleAvoidance(){
-  Backward();
-  _delay(0.5);
-  TurnRight();
-  _delay(0.5);
-  while(linefollower_63.readSensor()==0 && linefollower_64.readSensor()==0) {
-    motor_1.run(-0.3*255);
-    motor_9.run(-0.3*255);
-    motor_2.run(0.5*255);
-    motor_10.run(0.5*255);
-  }
-  }
 
 void Forward(){
   motor_1.run(-power*255);
@@ -169,15 +96,85 @@ void TurnRight(){
   motor_10.run(0);
 }
 
-void StrafeLeft(){
+void move_control(int16_t vx, int16_t vy, int16_t vw)
+{
+  int16_t foward_left_speed;
+  int16_t foward_right_speed;
+  int16_t back_left_speed;
+  int16_t back_right_speed;
 
-move_control(power * 255,0 / 100.0 * 255,0 / 100.0 * 255);
-_delay(float(T));
+  foward_left_speed = vy + vx + vw;
+  foward_right_speed = vy - vx - vw;
+  back_left_speed = vy - vx + vw;
+  back_right_speed = vy + vx - vw;
+
+  motor_foward_left_run(foward_left_speed);
+  motor_foward_right_run(foward_right_speed);
+  motor_back_left_run(back_left_speed);
+  motor_back_right_run(back_right_speed);
+}
+
+void _delay(float seconds) {
+  if(seconds < 0.0){
+    seconds = 0.0;
+  }
+  long endTime = millis() + seconds * 1000;
+  while(millis() < endTime) _loop();
+}
+
+void lineFollow(){
+  //for (int i =0; i<4; i++){
+  //rgbled_67.setColor(i, 100, 100, 100);
+  //rgbled_67.show();
+
+  //rgbled_68.setColor(i, 100, 100, 100);
+  //rgbled_68.show();
+  //}
+  double startTime = millis();  // 5 
+  int n = -1; 
+
+  if (linefollower_63.readSensor()==1 && linefollower_64.readSensor()==1) {
+    Forward();
+    Serial.println("both"); 
+  }
+
+  else if (linefollower_63.readSensor()==1 && linefollower_64.readSensor()==0){
+    TurnRight();
+    Serial.println("right"); 
+  }
+
+  else if (linefollower_63.readSensor()==0 && linefollower_64.readSensor()==1){
+    TurnLeft();
+    Serial.println("left") ;
+  }
+
+  else (linefollower_63.readSensor()==0 && linefollower_64.readSensor()==0){
+    Stop();
+    Serial.println("neither");
+  }
+}
+
+void ObstacleAvoidance(){
+  Backward();
+  _delay(0.5);
+  TurnRight();
+  _delay(0.5);
+  while(linefollower_63.readSensor()==0 && linefollower_64.readSensor()==0) {
+    motor_1.run(-0.3*255);
+    motor_9.run(-0.3*255);
+    motor_2.run(0.5*255);
+    motor_10.run(0.5*255);
+  }
+}
+
+void StrafeLeft(){
+  move_control(power * 255,0 / 100.0 * 255,0 / 100.0 * 255);
+  _delay(float(T));
 }
 
 void StrafeRight(){
-move_control(-power * 255,0 / 100.0 * 255,0 / 100.0 * 255);
-_delay(float(T));
+  move_control(-power * 255,0 / 100.0 * 255,0 / 100.0 * 255);
+  _delay(float(T));
 }
 
 void Stop(){
@@ -188,12 +185,13 @@ void Stop(){
 }
 void loop() {
   // put your main code here, to run repeatedly:
-    if (barrier_61.isBarried() || barrier_62.isBarried()) {
-      ObstacleAvoidance();
-    }
-    else {
-    lineFollow();
-  }  
+  // if (barrier_61.isBarried() || barrier_62.isBarried()) {
+  //   ObstacleAvoidance();
+  // }
+  // else {
+  //   lineFollow();
+  // }  
+  lineFollow();
 }
 
 
